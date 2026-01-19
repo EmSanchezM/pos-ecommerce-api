0f84c47bcb03bf1cb35aa62fb49d8f87314197db
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2026-01-19 12:38:42 -0600

    refactor(inventory): change structure of module inventory handlers

--- api-gateway/src/handlers/mod.rs
+++ api-gateway/src/handlers/mod.rs
@@ -2,12 +2,12 @@
 
 pub mod auth_handlers;
 pub mod cai_handlers;
-pub mod inventory_handlers;
+pub mod inventory;
 pub mod store_handlers;
 pub mod terminal_handlers;
 
 pub use auth_handlers::*;
 pub use cai_handlers::*;
-pub use inventory_handlers::*;
+pub use inventory::*;
 pub use store_handlers::*;
 pub use terminal_handlers::*;
