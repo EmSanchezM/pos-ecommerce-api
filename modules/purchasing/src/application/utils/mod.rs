f4a552a8a7e433f8be742f600b858408b11f8ac3
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2026-01-30 10:11:03 -0600

    feat(purchasing): add utility for generating vendor codes and update CreateVendorUseCase to use it

---
+++ modules/purchasing/src/application/utils/mod.rs
@@ -0,0 +1,7 @@
+//! Utility functions for the purchasing application layer.
+//!
+//! This module contains helper functions used by use cases.
+
+mod vendor_code_generator;
+
+pub use vendor_code_generator::generate_vendor_code_prefix;
